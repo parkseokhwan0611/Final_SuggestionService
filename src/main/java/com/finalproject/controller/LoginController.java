226a0f4f43667a2a9d491f27b03435a1890b7534
yeseul <yeseul0900@handong.ac.kr>
2022-12-15 15:56:43 +0900

    typo

--- src/main/java/com/finalproject/controller/LoginController.java
+++ src/main/java/com/finalproject/controller/LoginController.java
@@ -17,7 +17,7 @@ public class LoginController {
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
-        return "login";
+        return "board/login";
     }
 
     @RequestMapping(value = "/loginOk", method = RequestMethod.POST)
