6968ed8fc172bd16c46504100c2340d99f23361a
yeseul <yeseul0900@handong.ac.kr>
2022-12-15 14:09:48 +0900

    first

---
+++ src/main/java/com/finalproject/DAO/SuggestionDAO.java
@@ -0,0 +1,40 @@
+package com.finalproject.DAO;
+
+import com.finalproject.VO.SuggestionVO;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class SuggestionDAO{
+    @Autowired
+    SqlSession sqlSession;
+
+    public int insertSug(SuggestionVO vo){
+        int result = sqlSession.insert("Suggestion.insertSug",vo);
+        return result;
+    }
+
+    public int updateSug(SuggestionVO vo){
+        int result = sqlSession.insert("Suggestion.updateSug",vo);
+        return result;
+    }
+
+    public int deleteSug(int personID){
+        int result = sqlSession.delete("Suggestion.deleteSug", personID);
+        return result;
+    }
+
+    public SuggestionVO getSug(int personID){
+        SuggestionVO one = sqlSession.selectOne("Suggestion.getSug", personID);
+        return one;
+    }
+
+    public List<SuggestionVO> getSugList(){
+        List<SuggestionVO> list = sqlSession.selectList("Suggestion.getSugList");
+        return list;
+    }
+
+}
